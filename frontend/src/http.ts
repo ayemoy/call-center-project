32b204d7fae5ebef55dee4cc57762fe27a216ec5
ayemoy <ayelet431@gmail.com>
2025-05-23 00:19:04 +0300

    URL

--- frontend/src/http.ts
+++ frontend/src/http.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const http = axios.create({
-    baseURL: "http://localhost:3001", // backend base URL
+    // baseURL: "http://localhost:3001", // backend base URL
+    baseURL: process.env.REACT_APP_API_BASE_URL,
+
     headers: {
         "Content-Type": "application/json",
     },
--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,7 +1,9 @@
 // src/socket.ts
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001", {
+// const socket = io("http://localhost:3001", {
+const socket = io(process.env.REACT_APP_API_BASE_URL || "", {
+
   transports: ["websocket"],
   withCredentials: true,
 });
